978754bff384f5b72180cb0158905b89e1be20d3
Will <macuser@Mac-Users-MacBook-Pro.local>
2017-04-23 19:50:18 -0500

    Mobile Nav

--- static/templates/code-header.php
+++ static/templates/code-header.php
@@ -7,4 +7,7 @@
 	<link rel="stylesheet" href="/src/css/main.concat.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel='shortcut icon' href='/favicon.ico' type='image/x-icon'/ >
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+
 </head>
--- static/templates/header.php
+++ static/templates/header.php
@@ -1,19 +1,62 @@
 <header id="header">
-    <div class="container-fluid">
-      <nav>
+    <div>
+      <nav id="nav">
         <div class="row text-center">
             <div class="nav-item-container">
-              <div class="nav-item"><a href="#"> About Us</a> </div>
-              <div class="nav-item"><a href="#"> Our Services </a>  </div>
+              <div class="nav-item"><a href="#"> ABOUT US</a> </div>
+              <div class="nav-item"><a href="#"> OUR SERVICES </a>  </div>
               <div class="nav-item"><a href="#"> <img class="nav-logo" src="http://placehold.it/140x100"> </a>  </div>
-              <div class="nav-item"><a href="#"> Get a Quote </a>  </div>
-              <div class="nav-item"><a href="#"> Contact Us </a>  </div>
-              <div class="nav-item" id="nav-signUp"><a href="#"> Sign Up </a>  </div>
+              <div class="nav-item"><a href="#"> GET A QUOTE </a>  </div>
+              <div class="nav-item"><a href="#"> CONTACT US </a>  </div>
+              <div class="nav-item" id="nav-signUp"><a href="#"> SIGN UP </a>  </div>
 
             </div>
             <!--<div class="nav-signUp"><a href="#"> Sign Up </a>  </div>-->
 
         </div>
       </nav>
+
+
+    <nav id="mobile-nav">
+      <button class="hamburger hamburger--spin" type="button" id="mobile-nav-button">
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+      </button>
+      <ul class="mobile-nav-wrapper hidden fadeOut animated" id="mobile-nav-list">
+        <li> <a href="#"> ABOUT US</a></li>
+        <li> <a href="#"> OUR SERVICES </a></li>
+        <li> <a href="#"> GET A QUOTE </a></li>
+        <li><a href="#"> CONTACT US</a>  </li>
+        <li><a href="#"> SIGN UP</a>  </li>
+      </ul>
+    </nav>
+
+
+<script>
+
+var mobileNav = document.getElementById('mobile-nav');
+var mobileNavButton = document.getElementById('mobile-nav-button');
+var mobileNavList = document.getElementById('mobile-nav-list');
+mobileNavButton.onclick = function(event) {
+  console.log(this);
+  this.classList.toggle("is-active");
+  mobileNav.classList.toggle("mobile-nav-active");
+  mobileNavList.classList.toggle("hidden");
+  mobileNavList.classList.toggle("fadeIn");
+  mobileNavList.classList.toggle("fadeOut");
+
+
+
+
+
+
+
+ };
+
+
+
+</script>
+
     </div>
 </header>
