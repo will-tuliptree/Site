58d9f0b67e9cf15aa2d39461d3d0744e7bdab856
The King <TheKing@Thes-MacBook-Pro.local>
2017-05-17 17:29:01 -0500

    Updates

--- static/templates/home/module1.php
+++ static/templates/home/module1.php
@@ -8,7 +8,7 @@
     <div class="content">
         <img class="body-logo" src="images/body_logo.png"/>
         <h1>SERVICING CLAIMS, RECOVERING PREMIUMS, PROTECTING EMPLOYERS. </h1>
-        <p> At Tuliptree, we are dedicated to eliminating workers’ compensation insurance
+        <p class="fa"> At Tuliptree, we are dedicated to eliminating workers’ compensation insurance
             headaches and excess charges on policies. We come from the world of New York State
             Insurance Fund safety groups and have managed
             tens of millions in premium over a collective 35 years.</p>
--- static/templates/home/module2.php
+++ static/templates/home/module2.php
@@ -1,9 +1,55 @@
 <section id="about">
 <div class="container-slant">
   <div class="main">
-    <div class="main-container">
-        <h2>ABOUT US</h2>
-        <p>Oh God, my uncle. How am I ever gonna explain this? What?! In my experience, there is no such thing as luck. Kid, I've flown from one side of this galaxy to the other. I've seen a lot of strange stuff, but I've never seen anything to make me believe there's one all-powerful Force controlling everything. </p>
+    <div class="main-container content">
+        <h2 class="t-header">ABOUT US</h2>
+        <p>Our experience has been in providing cradle-to-grave service on claims, requiring years of
+            focus and follow through. Having consulted for clients outside our former safety groups,
+            we created our organization with the knowledge that our hard work, individualized attention,
+            and advocacy for our clients is exceptional, towering above the service provided by hundreds
+            or thousands of faceless carrier drones, and is aggressively competitive with the few
+            third-party representatives throughout New York. Realizing that there are thousands of
+            unrepresented and underrepresented businesses in the State of New York alone, we saw an
+            opportunity to help a great deal of managers,  administrators and owners save money and
+            apply their will power
+            to their business operations instead of workers’ comp headaches.
+         </p>
+         <h3> WHY TULIPTREE?</h3>
+         <p>
+             We have the deep resources, the advanced technical knowledge, and the experience at a
+              better price than anyone else in New York State (all else being equal).
+         </p>
+
+         <p>
+             Every company should have representation for workers’ comp claims and underwriting.
+              The carrier often assesses accidents too high and usually does not go back and
+              reconcile the assessment once the payments have been made and the case has been
+              retired and closed. To us, this is abuse of the client. Most business owners
+               would be shocked to know just how often this happens with claims
+                (about 35% of the time, based on our research).
+                 Getting representation is easy and will pay for itself several times
+                  over for the average company with claims. If you want constant,
+                  year-round service to maximize your savings and refunds,
+                   and want to achieve the lowest experience modification rating possible,
+                   we have a service for an ultra-low fee available. For a one-time premium
+                   refund and experience modification review, we have a no-risk service available.
+         </p>
+
+         <ul id="about-list">
+             <li>
+                 <div class="about-stat">42%</div>
+                 <span class="about-stat_detail">of our revised claims submissions resulted in a refund for our client </span>
+              </li>
+              <li>
+                  <div class="about-stat">8%</div>
+                  <span class="about-stat_detail">of our client's premium was recovered, on average. </span>
+               </li>
+
+               <li>
+                   <div class="about-stat">35%</div>
+                   <span class="about-stat_detail">of all workers' comp policies are refund-eligible. </span>
+                </li>
+         </ul>
     </div>
    </main>
 </div>
