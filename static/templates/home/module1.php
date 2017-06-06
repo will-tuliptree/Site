289dc79f7821f891a71c03663288f4e9a482bf00
The King <TheKing@Thes-MacBook-Pro.local>
2017-06-05 21:54:09 -0500

    Updates to new formate

--- static/templates/home/module1.php
+++ static/templates/home/module1.php
@@ -8,10 +8,10 @@
     <div class="content">
         <img class="body-logo" src="images/body_logo.png"/>
         <h1>SERVICING CLAIMS, RECOVERING PREMIUMS, PROTECTING EMPLOYERS. </h1>
-        <p class="fa"> At Tuliptree, we are dedicated to eliminating workers’ compensation insurance
-            headaches and excess charges on policies. We come from the world of New York State
-            Insurance Fund safety groups and have managed
-            tens of millions in premium over a collective 35 years.</p>
+        <i class="down-arrow fa"> </i>
+        <h2 class="t-header">ABOUT US</h2>
+        <p> </p>
+
     </div>
   </div>
 </section>
